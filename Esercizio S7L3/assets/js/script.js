c5cb7dd7c24da08bad0a8c103a7eec288ac8ca52
Kleo <kleoruda9@gmail.com>
2023-12-08 17:43:26 +0100

    Libreria

--- Esercizio S7L3/assets/js/script.js
+++ Esercizio S7L3/assets/js/script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 let card = document.createElement('div');
                 div.appendChild(card);
 
-                card.innerHTML = `<div class="card mx-1" style="width: 18rem; height: 99%">
+                card.innerHTML = `<div class="card mx-1" style="width: 18rem; height: 100%">
                 <img src="${e.img}" class="card-img-top" alt="Copertina">
                 <div class="card-body d-flex flex-column justify-content-between">
                     <div>
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div>
                       <p class="card-text">${e.price}€</p>
                       <button href="#" class="btn btn-primary">Compra ora</button>
-                      <button href="#" class="btn btn-danger">Scarta</button>
+                      <button href="#" class="btn btn-white btn-outline-danger">Scarta</button>
                     </div>
                 </div>
               </div>`
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     document.querySelector('#cards').addEventListener('click', (e) => {
-        if (e.target.tagName === 'BUTTON' && e.target.className === 'btn btn-danger') {
+        if (e.target.tagName === 'BUTTON' && e.target.className === 'btn btn-white btn-outline-danger') {
             let scarta = e.target.parentNode.parentNode.parentNode;
             scarta.remove();
         }
@@ -47,10 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target.tagName === 'BUTTON' && e.target.className === 'btn btn-primary') {
             let compra = e.target.parentNode.parentNode.parentNode;
 
-
-
-
-
             for (const ele of carrello) {
                 if (ele === compra) {
 
@@ -59,12 +55,22 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
 
-            let cart = document.querySelector('div.modal .modal-body')
-            cart.innerHTML += compra.innerHTML;
-            console.log(compra.innerHTML);
-            
 
-            
+
+            let cart = document.querySelector('div.modal .modal-body ul')
+            cart.style = 'list-style: none; padding: 0;'
+            let li = document.createElement('li');
+            li.classList.add('d-flex', 'my-2')
+            cart.appendChild(li);
+            li.innerHTML += compra.innerHTML;
+
+            cart.addEventListener('click', (e) => {
+                if (e.target.tagName === 'BUTTON' && e.target.className === 'btn btn-white btn-outline-danger') {
+                    let scarta = e.target.parentNode.parentNode.parentNode;
+                    scarta.remove();
+                }
+            })
+
             carrello.push(compra);
         }
     });
